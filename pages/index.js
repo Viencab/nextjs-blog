a969d6a37c06431c43a3f834d837092c56ea2c92
Viencab <143056485+Viencab@users.noreply.github.com>
2023-09-14 10:59:07 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>My name is Vien Arman C. Cabrera, My Hobbies are Playing basketball, editing, and playing video
-games are some of my pastimes. I particularly like listening to rap and some r&b. My favorite food is
-chicken, but I really enjoy experimenting with different kinds of food. I am a lifelong student who is open
-to new ideas and information. I like volunteering and supporting my community in my spare time.</p>
+        <p>My name is Vien Arman C. Cabrera, 10 Things That Require Zero Talent
+
+1. Being On Time
+2. Making An Effort
+3. Being High Energy
+4. Having A Positive Attitude
+5. Being Passionate
+6. Using Good Body Language
+7. Being Coachable
+8. Doing A Little Extra
+9. Being Prepared
+10. Having A Strong Work Ethic
+(This is a sample website - you’ll be building a site like this on our Next.js tutorial.).</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
